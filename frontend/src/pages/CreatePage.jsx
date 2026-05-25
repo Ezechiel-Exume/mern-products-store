f4bcc84602b95d1e02763db4e091f92ebcb5f2e9
Ezechiel <ezechiel.exume@gmail.com>
2026-05-25 11:45:16 -0400

    CreatePage created

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,5 +1,72 @@
+import { useColorMode } from "../components/ui/color-mode";
+import {
+  Box,
+  Button,
+  Container,
+  Heading,
+  Input,
+  VStack,
+} from "@chakra-ui/react";
+import { useState } from "react";
+
 const CreatePage = () => {
-  return <div>CreatePage</div>;
+  const [newProduct, setNewProduct] = useState({
+    name: "",
+    price: "",
+    image: "",
+  });
+  const { colorMode } = useColorMode();
+
+  const handleAddNewProduct = () => {
+    console.log(newProduct);
+  };
+  return (
+    <Container maxW={"container.sm"}>
+      <VStack>
+        <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
+          Create New Product
+        </Heading>
+        <Box
+          w={"full"}
+          p={6}
+          bg={colorMode === "light" ? "white" : "gray.800"}
+          rounded={"lg"}
+          shadow={"md"}
+        >
+          <VStack gap={4}>
+            <Input
+              placeholder="Product name"
+              name="name"
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, name: e.target.value })
+              }
+            />
+            <Input
+              placeholder="Price"
+              name="price"
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, price: e.target.value })
+              }
+            />
+            <Input
+              placeholder="Image URL"
+              name="image"
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, image: e.target.value })
+              }
+            />
+            <Button
+              colorPalette={"blue"}
+              w={"full"}
+              onClick={handleAddNewProduct}
+            >
+              Add Product
+            </Button>
+          </VStack>
+        </Box>
+      </VStack>
+    </Container>
+  );
 };
 
 export default CreatePage;
